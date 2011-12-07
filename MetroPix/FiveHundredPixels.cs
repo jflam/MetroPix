37221b87e669c6da295e8aa6fb04a796e5fd1ec1
John Lam <jlam@iunknown.com>
2011-12-06 16:48:08 -0800

    refactoring code to work more cleanly with rendered objects

--- MetroPix/FiveHundredPixels.cs
+++ MetroPix/FiveHundredPixels.cs
@@ -6,6 +6,16 @@ using Windows.Data.Json;
 
 namespace MetroPix
 {
+    public class PhotoSummary
+    {
+        public int Id { get; set; }
+        public Uri PhotoUri { get; set; }
+        public string Caption { get; set; }
+        public string Author { get; set; }
+        public int Votes { get; set; }
+        public double Rating { get; set; }
+    }
+
     public class FiveHundredPixels
     {
         private HttpClient _client = new HttpClient();
@@ -22,15 +32,21 @@ namespace MetroPix
 
         private FiveHundredPixels() { }
 
-        public async Task<JsonObject> GetFullSizePhoto(int id)
+        private Uri GetPhotoUri(string uri, int size)
         {
-            var requestUri = String.Format(GET_PHOTO_API, id, 4, CONSUMER_KEY);
-            var json = await _client.GetStringAsync(requestUri);
-            return JsonObject.Parse(json);
+            var directory = uri.Substring(0, uri.LastIndexOf('/'));
+            var photoUri = String.Format("{0}/{1}.jpg", directory, size);
+            return new Uri(photoUri);
         }
 
-        public async Task<List<PhotoSummary>> Query(string collection, int count = 20)
+        private List<PhotoSummary> _photos;
+
+        // TODO: fix caching
+        public async Task<List<PhotoSummary>> Query(string collection, int count = 20, int size = 4)
         {
+            if (_photos != null)
+                return _photos;
+
             var requestUri = String.Format(GET_PHOTOS_API, collection, 1, CONSUMER_KEY, count);
             var json = await _client.GetStringAsync(requestUri);
             var photos = JsonObject.Parse(json)["photos"].GetArray();
@@ -38,34 +54,34 @@ namespace MetroPix
             for (uint i = 0; i < photos.Count; i++)
             {
                 var photo = photos.GetObjectAt(i);
-                
+
                 int id = Convert.ToInt32(photo["id"].GetNumber());
                 string caption = photo["name"].GetString();
                 string author = photo["user"].GetObject()["fullname"].GetString();
                 double rating = photo["rating"].GetNumber();
                 int votes = Convert.ToInt32(photo["rating"].GetNumber());
-                Uri uri = new Uri(photo["image_url"].GetString());
+                Uri uri = GetPhotoUri(photo["image_url"].GetString(), size);
 
-                var item = new PhotoSummary()
+                result.Add(new PhotoSummary
                 {
                     Id = id,
-                    Author = author,
                     Caption = caption,
-                    Rating = rating,
-                    Votes = votes,
+                    Author = author,
                     PhotoUri = uri,
-                };
-                result.Add(item);
+                    Rating = rating,
+                    Votes = votes
+                });
             }
+            _photos = result;
             return result;
         }
 
-        public Uri GetPhotoUri(PhotoSummary photo, int size)
+        // TODO: get rid of JsonObject
+        public async Task<JsonObject> GetFullSizePhoto(int id)
         {
-            var url = photo.PhotoUri.ToString();
-            var directory = url.Substring(0, url.LastIndexOf('/'));
-            var photoUri = String.Format("{0}/{1}.jpg", directory, size);
-            return new Uri(photoUri);
+            var requestUri = String.Format(GET_PHOTO_API, id, 4, CONSUMER_KEY);
+            var json = await _client.GetStringAsync(requestUri);
+            return JsonObject.Parse(json);
         }
 
         // BUGBUG: I have to create a singleton FiveHundredPixels object because I can't do an HTTP request
--- MetroPix/FrontPage.xaml.cs
+++ MetroPix/FrontPage.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
@@ -10,72 +11,85 @@ namespace MetroPix
 {
     public sealed partial class FrontPage : Page
     {
-        // Note that the .ctor fires here even if we are returning from a different page -- where do we store state?
         public FrontPage()
         {
             InitializeComponent();
         }
 
-        // TODO: figure out how to do the right kind of data binding here?! What kind of control do I need? A ListView?
-        private async void LoadPhotos()
-        {
-            var photos = await FiveHundredPixels.Site.Query("popular", 20);
-            foreach (var photo in photos)
-            {
-                var photoUri = FiveHundredPixels.Site.GetPhotoUri(photo, 4);
-                var grid = new Grid();
-
-                var image = new Image();
-                image.Margin = new Thickness(5, 0, 5, 0);
-                image.Source = new BitmapImage(photoUri);
-                image.Height = 650;
-                image.Tapped += (sender, args) =>
-                {
-                    // BUGBUG: all event handlers are not wired up when I go back!
-                    Frame.Navigate(typeof(SinglePicture), photo.Id);
-                };
-
-                // Make a text block with the caption
-                var canvas = new Canvas();
-                canvas.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Bottom;
-                canvas.Width = image.Width;
-                canvas.Margin = new Thickness(5, 0, 5, 0);
-                canvas.Height = 60;
-                canvas.Background = new SolidColorBrush(Colors.Black);
-                canvas.Opacity = 0.8;
-
-                var text = new TextBlock();
-                text.Margin = new Thickness(10, 0, 0, 0);
-                text.FontFamily = new Windows.UI.Xaml.Media.FontFamily("Segoe UI");
-                text.FontWeight = FontWeights.Bold;
-                text.Foreground = new SolidColorBrush(Colors.White);
-                text.FontSize = 24;
-                text.Text = photo.Caption;
-
-                var name = new TextBlock();
-                name.Margin = new Thickness(10, 0, 0, 0);
-                name.FontFamily = new Windows.UI.Xaml.Media.FontFamily("Segoe UI");
-                name.Foreground = new SolidColorBrush(Colors.White);
-                name.FontSize = 12;
-                name.Text = photo.Author;
-
-                var left = new StackPanel();
-                left.Orientation = Orientation.Vertical;
-                left.Children.Add(text);
-                left.Children.Add(name);
-
-                canvas.Children.Add(left);
-
-                grid.Children.Add(image);
-                grid.Children.Add(canvas);
-
-                Photos.Children.Add(grid);
-            }
-        }
-
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             LoadPhotos();
+        }
+
+        private Grid RenderPhotoWithCaption(PhotoSummary photo, int index)
+        {
+            var grid = new Grid();
+
+            var bitmap = new BitmapImage(photo.PhotoUri);
+            var image = new Image();
+            image.Margin = new Thickness(5, 0, 5, 0);
+            image.Source = bitmap;
+            image.Height = 650;
+            image.Tag = photo.Id;
+
+            // Make a text block with the caption
+            var canvas = new Canvas();
+            canvas.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Bottom;
+            canvas.Width = image.Width;
+            canvas.Margin = new Thickness(5, 0, 5, 0);
+            canvas.Height = 60;
+            canvas.Background = new SolidColorBrush(Colors.Black);
+            canvas.Opacity = 0.8;
+
+            var text = new TextBlock();
+            text.Margin = new Thickness(10, 0, 0, 0);
+            text.FontFamily = new Windows.UI.Xaml.Media.FontFamily("Segoe UI");
+            text.FontWeight = FontWeights.Bold;
+            text.Foreground = new SolidColorBrush(Colors.White);
+            text.FontSize = 24;
+            text.Text = photo.Caption;
+
+            var name = new TextBlock();
+            name.Margin = new Thickness(10, 0, 0, 0);
+            name.FontFamily = new Windows.UI.Xaml.Media.FontFamily("Segoe UI");
+            name.Foreground = new SolidColorBrush(Colors.White);
+            name.FontSize = 12;
+            name.Text = photo.Author;
+
+            var left = new StackPanel();
+            left.Orientation = Orientation.Vertical;
+            left.Children.Add(text);
+            left.Children.Add(name);
+
+            canvas.Children.Add(left);
+
+            grid.Children.Add(image);
+            grid.Children.Add(canvas);
+
+            return grid;
+        }
+
+        private async void LoadPhotos()
+        {
+            var photos = await MetroPix.FiveHundredPixels.Site.Query("popular", 50, 4);
+
+            for (int i = 0; i < photos.Count; i++)
+            {
+                Photos.Children.Add(RenderPhotoWithCaption(photos[i], i));
+            }
+        }
+
+        private void ScrollViewer_Tapped_1(object sender, TappedRoutedEventArgs e)
+        {
+            var image = e.OriginalSource as Image;
+            var obj = e.OriginalSource;
+            if (image != null)
+            {
+                if (image.Tag != null)
+                {
+                    Frame.Navigate(typeof(SinglePicture), image.Tag);
+                }
+            }
         }
     }
 }
